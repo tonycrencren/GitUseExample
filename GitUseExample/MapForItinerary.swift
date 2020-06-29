88af8af3473a872b3dac80d745f5d5f57133b141
hackchains <hackchains@hackchainss-mbp>
2020-06-28 19:35:19 -0700

    map_feature commit #1

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by hackchains on 6/28/20.
+//  Copyright © 2020 hackchains. All rights reserved.
+//
+
+import Foundation
+
+class MapForItinerary: NSObject {
+    
+}
